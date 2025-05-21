126c6becf4428549742842bf0698b5ecef34cf0a
dev.antune$ <thiagoantunes0101@gmail.com>
2025-05-21 09:47:31 -0300

    sobe tudo ae poh kkkk

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,9 +31,9 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
-        ctaText={'shop now'}
+        title={'Cada livro é uma viagem sem sair do lugar.'}
+        subtitle={'Palavras que tocam, histórias que transformam.'}
+        ctaText={'Quero saber mais!'}
         ctaAction={goToShop}
       />
 
